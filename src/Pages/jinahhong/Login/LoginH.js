fb02aa195f694a8d78cc79afef8158e9564022d0
jinahHong <jinah2754@gmail.com>
2021-04-02 18:06:43 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/Pages/jinahhong/Login/LoginH.js
+++ src/Pages/jinahhong/Login/LoginH.js
@@ -3,6 +3,29 @@ import { Link } from "react-router-dom";
 import "./LoginH.scss";
 
 class LoginH extends React.Component {
+  constructor() {
+  super()
+    this.state = {
+      id: "",
+      pw: "",
+      backColor: ""
+    }
+  }
+  consoleID = (event) => {
+    this.setState({
+      id: event.target.value
+    })
+  }
+
+  consolePW = (event) => {
+    this.setState({
+      pw: event.target.value
+    })
+  }
+
+  showValue =() => {
+    this.props.history.push('/main')
+  }
   render() {
     return (
       <section>
@@ -14,8 +37,9 @@ class LoginH extends React.Component {
                 type="text"
                 id="email_password"
                 placeholder="전화번호,사용자 이름 또는 이메일"
-              />
-              <input type="text" id="login_password" placeholder="비밀번호" />
+                onChange={this.consoleID}/>
+              <input type="text" id="login_password" placeholder="비밀번호"
+              onChange={this.consolePW} />
             </div>
             <button className="login_btn" disabled>
               <Link to="/MainH">로그인</Link>
