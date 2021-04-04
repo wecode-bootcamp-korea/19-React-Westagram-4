c8b7df5ae04bc658eeddccb1ae56b08e282fccc4
itssweetrain <76150928+itssweetrain@users.noreply.github.com>
2021-04-04 15:58:41 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/Pages/danbipark/Login/LoginP.js
+++ src/Pages/danbipark/Login/LoginP.js
@@ -5,6 +5,28 @@ import "../../../Styles/Reset.scss";
 import { Link } from "react-router-dom";
 
 class Login extends React.Component {
+  state = {
+    loginId: "",
+    loginPw: "",
+    IsDisabled: true,
+  };
+
+  handleValueInput = (e) => {
+    const { id, value } = e.target;
+    this.setState({ [id]: value }, () => {
+      this.checkLogin();
+    });
+  };
+
+  checkLogin = () => {
+    this.setState({
+      IsDisabled:
+        this.state.loginPw.length >= 5 && this.state.loginId.includes("@")
+          ? false
+          : true,
+    });
+  };
+
   render() {
     return (
       <div className="Login">
@@ -12,14 +34,24 @@ class Login extends React.Component {
           <header>Westagram</header>
           <div className="input-container">
             <div className="id-container">
-              <input type="text" id="id-input" />
-              <label for="id-input">전화번호,사용자 이름 또는 이메일</label>
+              <input
+                type="text"
+                id="loginId"
+                value={this.state.loginId}
+                onChange={this.handleValueInput}
+              />
+              <label for="loginId">전화번호,사용자 이름 또는 이메일</label>
             </div>
             <div className="password-container">
-              <input type="password" id="password-input" />
-              <label for="password-input">비밀번호</label>
+              <input
+                type="password"
+                id="loginPw"
+                value={this.state.loginPw}
+                onChange={this.handleValueInput}
+              />
+              <label for="loginPw">비밀번호</label>
             </div>
-            <button className="login-button">
+            <button className="login-button" disabled={this.state.IsDisabled}>
               <Link to="/MainP" className="goToMain">
                 로그인
               </Link>
