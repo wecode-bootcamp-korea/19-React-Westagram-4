0dca41145a4df2cfababdb5722af45452d296057
Eddington524 <iamsam8687@gmail.com>
2021-04-04 14:25:49 +0900

    Add:Mission 1 - 사용자 입력 데이터 저장기능 구현

--- src/Pages/saemijung/Login/LoginJ.js
+++ src/Pages/saemijung/Login/LoginJ.js
@@ -3,9 +3,30 @@ import React, { Component } from 'react';
 import "./LoginJ.scss";
 
 class LoginJ extends Component {
+  constructor(){
+    super();
+    this.state ={
+      idInputData : "",
+      pwInputData : "",
+    }
+  }
   goTomain = () =>{ 
     this.props.history.push('/MainJ');
     }
+  handleIdInput = (event)=>{
+    // console.log(event.target.value);
+    this.setState({
+      idInputData :event.target.value,
+    })
+  }
+  handlePwInput = (event)=>{
+    // console.log(event.target.value);
+    this.setState({
+      pwInputData :event.target.value,
+    })
+  }
+
+  
 
   render() {
     return (
@@ -14,13 +35,13 @@ class LoginJ extends Component {
         <div className="logo">
             <h1>westagram</h1>
         </div>
-        <section className="login_form">
-            <input className="login_id" type="text" placeholder="전화번호,사용자 이름 또는 이메일" />
-            <input className="login_pw" type="text" placeholder="비밀번호" />
-            {/* <Link to ="/main"> */}
-            <button type ="button" className="login_btn" onClick={this.goTomain}>로그인</button>
-            {/* </Link> */}
-            <div className="forget_pw">
+      <section className="login_form">
+        <input className="login_id" onChange={this.handleIdInput} type="text" placeholder="전화번호,사용자 이름 또는 이메일" />
+        <input className="login_pw" onChange={this.handlePwInput} type="text" placeholder="비밀번호" />
+        {/* <Link to ="/main"> */}
+        <button type ="button" className="login_btn" onClick={this.goTomain}>로그인</button>
+        {/* </Link> */}
+        <div className="forget_pw">
                 <a className="forget_link" target="_blank" href="noreferrer" >비밀번호를 잊으셨나요?</a>
             </div>
         </section>
