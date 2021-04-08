d3e86d2b4142412f03c98a4e8b8c1a4ed785d3fd
jinahHong <jinah2754@gmail.com>
2021-04-08 11:22:24 +0900

    Login 수정

--- src/Pages/jinahhong/Login/LoginH.js
+++ src/Pages/jinahhong/Login/LoginH.js
@@ -11,21 +11,63 @@ class LoginH extends React.Component {
       backColor: ""
     }
   }
-  handleIdInput = (event) => {
-    this.setState({
-      idInputValue: `${event.target.value}`,
-    });
-  };
+  // handleIdInput = (event) => {
+  //   this.setState({
+  //     idInputValue: `${event.target.value}`,
+  //   });
+  // };
 
-  handlePwInput = (event) => {
-    this.setState({
-      pwInputValue: `${event.target.value}`,
-    });
-  };
+  // handlePwInput = (event) => {
+  //   this.setState({
+  //     pwInputValue: `${event.target.value}`,
+  //   }); 
+  // };
+  
+  handleInput = (e) => {
+    const{ className } = e.target;
+    const { value } =e.target;
+    className === "email_password"
+      ? this.setState({
+        idInputValue: value,
+      })
+      :this.setState({
+        pwInputValue: value,
+      });
+  }
+
 
   showValue =() => {
     this.props.history.push('/main')
   }
+
+    goTomain = () => {
+      // console.log("클릭")
+      // this.props/history.push("/MainH");
+        fetch("http://10.58.0.137:8000/users/signin", {
+          method: "POST",
+          // body: JSON.stringify({
+          //   email: "jinah@ja.com",
+          //   password: "12345678",
+          // }),
+          body: JSON.stringify({
+            email: this.state.idInputValue,
+            password: this.state.pwInputValue,
+            // name: "luna",
+            // phone_num: "01081405785"
+          }),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data)
+          if(data["MESSAGE"] === "SUCCESS") {
+            alert("로그인 성공")
+          } alert ("로그인에 일치하지 않습니다!")
+          localStorage.setItem("token", data.Token);
+        });  
+    };
+
+
+    
   render() {
     return (
       <section>
@@ -46,20 +88,24 @@ class LoginH extends React.Component {
               onChange={this.handlePwInput} />
             </div>
             <button
+            onClick={this.goTomain}
             id="login_btn"
-            className={
-              this.state.idInputValue.indexOf("@") !== -1 &&
-              this.state.pwInputValue.length > 5
-              ? "changeButtonColor"
-              : "normalButtonColor"
-            }
-            disabled={
-              this.state.idInputValue.indexOf("@") !== -1 &&
-              this.state.pwInputValue.length > 5
-                ? false
-                : true
-            }>
+            // className={
+            //   this.state.idInputValue.indexOf("@",".") !== -1 &&
+            //   this.state.pwInputValue.length > 8
+            //   ? "changeButtonColor"
+            //   : "normalButtonColor"
+            // }
+            // disabled={
+            //   this.state.idInputValue.indexOf("@",".") !== -1 &&
+            //   this.state.pwInputValue.length > 8
+            //     ? false
+            //     : true
+            // }
+            >
               <Link to="/MainH" style={{color:'white'}}>로그인</Link>
+              {/* 로그인 */}
+
             </button>
 
             <div className="rePassword_btn">
